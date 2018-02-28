db0d7578290b5b8bf6baa1d5076fe70d002dadd4
Jeliozver <jelioradoev@gmail.com>
2018-02-28 16:15:21 +0200

    Added solutions for Classes and Members lab

---
+++ 07-Classes-and-Members-Lab/P03-Get-Persons/getPersons.js
@@ -0,0 +1,24 @@
+function getPersons() {
+    class Person {
+        constructor(firstName, lastName, age, email) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+        }
+
+        toString() {
+            return `${this.firstName} ${this.lastName} (age: ${this.age}, email: ${this.email})`
+        }
+    }
+
+    let result = [];
+    result.push(new Person('Maria', 'Petrova', 22, 'mp@yahoo.com'));
+    result.push(new Person('SoftUni'));
+    result.push(new Person('Stephan', 'Nikolov', 25));
+    result.push(new Person('Peter', 'Kolev', 24, 'ptr@gmail.com'));
+
+    return result;
+}
+
+console.log(getPersons().join(', '));
---
+++ 07-Classes-and-Members-Lab/P06-Cards/cards.js
@@ -0,0 +1,57 @@
+let result = ((() => {
+    const Suits = {
+        'SPADES': "\u2660", // ♠
+        'HEARTS': "\u2665", // ♥
+        'DIAMONDS': "\u2666", // ♦
+        'CLUBS': "\u2663" // ♣
+    };
+
+    const validFaces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    const validSuits = ["\u2660", "\u2665", "\u2666", "\u2663"];
+
+    class Card {
+        constructor(face, suit) {
+            this.face = face;
+            this.suit = suit;
+        }
+
+        get face() {
+            return this._face;
+        }
+
+        set face(value) {
+            if (!validFaces.includes(value)) {
+                throw new Error('Invalid card face!');
+            }
+            this._face = value;
+        }
+
+        get suit() {
+            return this._suit;
+        }
+
+        set suit(value) {
+            if (!validSuits.includes(value)) {
+                throw new Error('Invalid card suit!');
+            }
+            this._suit = value;
+        }
+
+        toString() {
+            return this._face + this._suit;
+        }
+    }
+
+    return {
+        Suits:Suits,
+        Card:Card
+    }
+}))();
+
+let Card = result.Card;
+let Suits = result.Suits;
+let card = new Card('Q', Suits.CLUBS);
+console.log(card.toString());
+card.face = 'A';
+card.suit = Suits.HEARTS;
+console.log(card.toString());
