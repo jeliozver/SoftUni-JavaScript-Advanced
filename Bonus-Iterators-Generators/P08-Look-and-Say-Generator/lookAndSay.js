62f084ae9207dff2a68984995485701b3bb23911
Jeliozver <jelioradoev@gmail.com>
2018-03-12 08:40:43 +0200

    Added solutions for Iterators and Generators Bonus problems

--- Bonus-Iterators-Generators/P08-Look-and-Say-Generator/lookAndSay.js
+++ Bonus-Iterators-Generators/P08-Look-and-Say-Generator/lookAndSay.js
@@ -1,19 +1,33 @@
-function * lookAndSay(start) {
-    let nextElement;
-    //TODO
-    yield nextElement;
+function* lookAndSay(start) {
+    let seed = start.toString();
+
+    while (true) {
+        let nextElement = '';
+        let count = 1;
+        for (let i = 0; i < seed.length; i++) {
+                if (seed[i] === seed[i + 1]) {
+                    count++;
+                } else {
+                    nextElement += `${count}${seed[i]}`;
+                    count = 1;
+                }
+        }
+
+        seed = nextElement;
+        yield nextElement;
+    }
 }
 
 let lookSequence = lookAndSay(1);
-console.log(lookSequence.next().value);
-console.log(lookSequence.next().value);
-console.log(lookSequence.next().value);
-console.log(lookSequence.next().value);
-console.log(lookSequence.next().value);
-
+console.log(lookSequence.next().value); // 11
+console.log(lookSequence.next().value); // 21
+console.log(lookSequence.next().value); // 1211
+console.log(lookSequence.next().value); // 111221
+console.log(lookSequence.next().value); // 312211
+console.log('------------');
 let lookSequence2 = lookAndSay(113);
-console.log(lookSequence2.next().value);
-console.log(lookSequence2.next().value);
-console.log(lookSequence2.next().value);
-console.log(lookSequence2.next().value);
-console.log(lookSequence2.next().value);
+console.log(lookSequence2.next().value); // 2113
+console.log(lookSequence2.next().value); // 1222113
+console.log(lookSequence2.next().value); // 11222113
+console.log(lookSequence2.next().value); // 21322113
+console.log(lookSequence2.next().value); // 121113222113
